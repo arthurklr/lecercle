8e940e0741663e3a8105056d79ca8926e4ce743a
arthurklr_ <arthur.k2104@gmail.com>
2024-09-10 14:31:57 +0200

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+// src/pages/Home.js
+import React from 'react';
+import Carousel from '../components/Carousel';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Bienvenue au Cinéma</h1>
+      <Carousel />
+    </div>
+  );
+}
+
+export default Home;
