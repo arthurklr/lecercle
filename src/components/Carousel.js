7822ad5f1960708caf6668a3d80afc2a032bd951
arthurklr_ <arthur.k2104@gmail.com>
2024-09-11 09:27:54 +0200

    Update Carousel.js

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,27 +1,15 @@
-// src/components/Carousel.js
-import React, { useEffect, useState } from 'react';
-import './Carousel.css'; // Styles spécifiques au carrousel
+import React from 'react';
+import './Carousel.css';
 
-function Carousel() {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    // Charger les données depuis le fichier JSON
-    fetch('/path/to/films.json')  // Mets le bon chemin pour le fichier JSON
-      .then((response) => response.json())
-      .then((data) => setMovies(data.movies));
-  }, []);
-
-  return (
-    <div className="carousel">
-      {movies.map((movie) => (
-        <div className="carousel-item" key={movie.id}>
-          <img src={movie.picture.medium} alt={movie.title} />
-          <h2>{movie.title}</h2>
-        </div>
-      ))}
-    </div>
-  );
-}
+const Carousel = ({ movies }) => (
+  <div className="carousel">
+    {movies.map((movie) => (
+      <div className="carousel-item" key={movie.id}>
+        <img src={movie.picture.big} alt={movie.title} />
+        <h3>{movie.title}</h3>
+      </div>
+    ))}
+  </div>
+);
 
 export default Carousel;
