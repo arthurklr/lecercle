6c20caed56032a150136a82009871a864491f0fa
arthurklr_ <arthur.k2104@gmail.com>
2024-09-11 09:27:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Films from './pages/Films';
-import './App.css';  // Ajoute les styles généraux ici
+import Header from './components/Header';
+import './styles/App.css';
 
 function App() {
   return (
     <Router>
-      <header className="header">
-        <nav>
-          <ul>
-            <li><Link to="/">Accueil</Link></li>
-            <li><Link to="/about">À propos</Link></li>
-            <li><Link to="/films">Films</Link></li>
-          </ul>
-        </nav>
-      </header>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
